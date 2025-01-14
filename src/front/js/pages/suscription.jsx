764118120015d5f663a1b4f34824b5379beeca8d
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2025-01-14 20:19:47 +0000

    subscripcion

--- src/front/js/pages/suscription.jsx
+++ src/front/js/pages/suscription.jsx
@@ -7,7 +7,38 @@ export const Suscription = () => {
 
     return (
        <>
-       <h1>Suscription</h1>
+       <div className="card-container">
+  <div className="card basic">
+    <div className="icon"></div>
+    <h2>Básico</h2>
+        <ul>
+                <li>Descuentos estándar</li>
+                <li>Tarifas de envío regulares</li>
+                <li>Visibilidad limitada de tus productos</li>
+                <li>Acceso a contenido general</li>
+                <li>Soporte y asistencia estándar</li>
+                <li>Acceso al FAQ</li>
+        </ul>
+    <p>Gratis</p>
+  </div>
+
+  <div className="card premium">
+    <div className="icon crown"></div>
+    <h2>PREMIUM BOSS</h2>
+        <ul>
+                <li>Descuentos exclusivos</li>
+                <li>Envios gratuitos</li>
+                <li>Mejora de la visibilidad de sus productos</li>
+                <li>Acceso anticipado a contenido exclusivo</li>
+                <li>Soporte y asistencia mejorado</li>
+        </ul>
+    <p className="price">9.99€<span>/mes</span></p>
+    <button>¡Únete!</button>
+  </div>
+</div>
+
+
+
        </>
     );
 };
