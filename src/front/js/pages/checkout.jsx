6575ba19e1092167808e040adc5985044dd1112a
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2025-01-20 20:31:01 +0000

    added link to suscripcion

---
+++ src/front/js/pages/checkout.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import "../../styles/checkout.css";
+
+export const Checkout = () => {
+
+    return (
+        <>
+        <h1>checkout</h1>
+        </>
+    )
+}
